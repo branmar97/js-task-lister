a22080a59e99c52007b420d4e1d5d7e4799ee6cb
8bitbran <brandonmar97@gmail.com>
2020-12-15 20:46:15 -0500

    Add button selectors

---
+++ app.js
@@ -0,0 +1,4 @@
+const addBtn = document.getElementById('add-task-btn');
+const clearBtn = document.getElementById('clear-tasks-btn');
+const list = document.querySelector('ul.collection');
+const delBtns = document.querySelectorAll('.delete-item');
